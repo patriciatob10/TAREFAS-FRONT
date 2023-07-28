b652865d60faef68ae5a107681d8445de6cb5ab7
patriciatobias.facul@gmail.com <patriciatobias.facul@gmail.com>
2023-07-27 21:58:28 -0300

    v.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
+import { AdicionarTarefaComponent } from './adicionar-tarefa/adicionar-tarefa.component';
+import { FormTarefaComponent } from './form-tarefa/form-tarefa.component';
+import { CardTarefaComponent } from './card-tarefa/card-tarefa.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaTarefasComponent,
+    AdicionarTarefaComponent,
+    FormTarefaComponent,
+    CardTarefaComponent,
+    RodapeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card-tarefa/card-tarefa.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-tarefa',
+  templateUrl: './card-tarefa.component.html',
+  styleUrls: ['./card-tarefa.component.css']
+})
+export class CardTarefaComponent {
+
+  @Input() tarefa: any;
+
+  
+}
